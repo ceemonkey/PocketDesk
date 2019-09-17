c6fa03056524668bb278cfd915fca093932fcf5a
Christian Cosas <32409552+ceemonkey@users.noreply.github.com>
2019-09-17 06:38:18 -0400

    Switched to Pocket-Home-Bismuth
    Pocket-Home-Bismuth is under active development!

--- Scripts/pockethome.sh
+++ Scripts/pockethome.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # PocketHOME
-# Rewritten to retrieve by git instead of apt repository
+# Rewritten to retrieve from centuryglass' Pocket-Home-Bismuth github
+# instead of marshmallow's apt repository
 
-git clone --recursive https://github.com/o-marshmallow/PocketCHIP-pocket-home/
-make
-make devinstall
+wget https://github.com/centuryglass/Pocket-Home-Bismuth/releases/download/v0.1.1/pocket-home_0.1.1_armhf.deb
+dpkg -i pocket-home_0.1.1_armhf.deb
 
-# Install AllGrey's config.json
-mkdir /home/chip/.pocket-home/
-wget -O /home/chip/.pocket-home/config.json https://raw.githubusercontent.com/AllGray/PocketDesk/master/files/config.json
-sudo chown -R chip: .pocket-home/
+# Install AllGrey's config.json; deprecated and commented out to see if this works
+# mkdir /home/chip/.pocket-home/
+# wget -O /home/chip/.pocket-home/config.json https://raw.githubusercontent.com/AllGray/PocketDesk/master/files/config.json
+# sudo chown -R chip: .pocket-home/
